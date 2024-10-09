32707ae6755606a11e733bb7280b6859c82685d2
marieke-thomas <32941729+marieke-thomas@users.noreply.github.com>
2024-10-08 22:29:00 -0400

    Add files via upload

---
+++ countdown_bug.java
@@ -0,0 +1,8 @@
+public class countdown_bug {
+    public static void main(String[] args){
+        for (int i = 10; i > 1; i--) {
+            System.out.println(i);
+            System.out.println("Happy New Year!");
+        }
+    }
+}
